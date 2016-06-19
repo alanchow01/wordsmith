a46b4dde3ba1315a39e0a07170c518a0547ebed9
alanchow01 <hello@alanchow.ca>
2016-06-18 17:23:40 -0700

    initial commit

---
+++ wp-content/themes/vdf/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wordsmith
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'feistner' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'feistner' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'feistner' ), 'feistner', '<a href="http://www.alanchow.ca" rel="designer">Alan Chow</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
